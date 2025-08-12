fe630cf53e92c331ce990dda763acda32e58307b
Yassine Khiari <tasssannas@gmail.com>
2025-08-12 03:33:25 +0100

    'final'

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,20 +20,19 @@ import { WebhookModule } from './webhook/webhook.module';
   imports: [
     ScheduleModule.forRoot(),
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-     useFactory: (configService: ConfigService) => ({
-  type: 'postgres',
-  host: configService.get<string>('POSTGRES_HOST'),
-  port: Number(configService.get<string>('POSTGRES_PORT')) || 5432,
-  username: configService.get<string>('POSTGRES_USER'),
-  password: configService.get<string>('POSTGRES_PASSWORD'),
-  database: configService.get<string>('POSTGRES_DB'),
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: true,
+TypeOrmModule.forRootAsync({
+  imports: [ConfigModule],
+  useFactory: (configService: ConfigService) => ({
+    type: 'postgres',
+    url: configService.get<string>('DATABASE_URL'),
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+    ssl: false, // no need since sslmode=require is in the URL
+  }),
+  inject: [ConfigService],
 }),
-      inject: [ConfigService],
-    }),
+
+
     UsersModule,
     SetupModule,
     AuthModule,
